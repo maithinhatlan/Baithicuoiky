593fb8237428e77cf153583d747f3d13c9c22b78
maithinhatlan <you@example.com>
2022-01-14 10:17:19 +0700

    upnewfile

---
+++ Bai4.py
@@ -0,0 +1,9 @@
+import re
+chuoi = ', '
+thongtin = input("Nhập vào chuỗi tên và mã sinh viên: ")
+giatri = chuoi.join(thongtin)
+danhsach=giatri.split(",")
+tuple=tuple(danhsach)
+print ("Danh sách là: ",danhsach, end=" ")
+print("")
+print ("Tuple là: ",tuple, end=" ")
